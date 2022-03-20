9967240ce37221e5471ad9a330826f7bc882d1f4
dong-geon-Lee <ch9716@naver.com>
2022-03-20 20:39:13 +0900

    Login completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
@@ -10,7 +12,9 @@ const App = () => {
       {/* <Home></Home> */}
       {/* <ProductList></ProductList> */}
       {/* <Product></Product> */}
-      <Register></Register>
+      {/* <Register></Register> */}
+      {/* <Login></Login> */}
+      <Cart></Cart>
     </>
   );
 };
