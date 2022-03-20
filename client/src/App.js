93557fed07284ff5e60d0f20b13777b6b71c3649
dong-geon-Lee <ch9716@naver.com>
2022-03-20 10:37:19 +0900

    Navbar completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Home></Home>
+    </>
   );
 };
 
