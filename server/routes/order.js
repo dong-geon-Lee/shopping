39e49789fab17624cc3f4b86fa52005947754ae2
dong-geon-Lee <ch9716@naver.com>
2022-03-21 15:26:15 +0900

    completed Routes/Order

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,7 +1,65 @@
 const router = require("express").Router();
+const Order = require("../models/Order");
+const {
+  verifyTokenAndAdmin,
+  verifyToken,
+  verifyTokenAndAuthorization,
+} = require("./verifyToken");
 
-router.get("/", (req, res) => {});
+router.post("/", verifyToken, async (req, res) => {
+  // user, auth를 제외하면 req.body로 간단하게 표현했다
+  const newOrder = new Order(req.body);
 
-router.post("/", (req, res) => {});
+  try {
+    const savedOrder = await newOrder.save();
+
+    res.status(200).json(savedOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    res.status(200).json(updatedOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json("Order has been deleted...");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const orders = await Order.find({ userId: req.body.userId });
+
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const orders = await Order.find();
+
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
