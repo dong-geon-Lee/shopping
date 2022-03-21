91e6804597fabbc1fa8746b114a16bc0b5a87c20
dong-geon-Lee <ch9716@naver.com>
2022-03-21 13:26:51 +0900

    completed auth & user

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/user", require("./routes/user"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/users", require("./routes/user"));
 
 app.listen(port, () => console.log(`Server is running on port:${port}`));
---
+++ server/routes/auth.js
@@ -0,0 +1,74 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+
+  try {
+    const savedUser = await newUser.save();
+
+    console.log(savedUser);
+
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    if (!user) res.status(401).json("Wrong credentials!");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    console.log(hashedPassword, "hash");
+    console.log(originalPassword, "pass");
+
+    if (originalPassword !== req.body.password) {
+      res.status(401).json("Wrong credentials!");
+    }
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
+    console.log(accessToken.toString(), "token");
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accessToken });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
+
+// const newUser = await User.create({
+//   username: req.body.username,
+//   email: req.body.email,
+//   password: req.body.password,
+// });
+
+// res.status(201).json(newUser);
