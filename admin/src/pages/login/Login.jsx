58525b3b88b46b3751404fab804f2aa551c103ac
dong-geon-Lee <ch9716@naver.com>
2022-03-23 22:21:11 +0900

    admin page

---
+++ admin/src/pages/login/Login.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../redux/apiCall.js";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    login(dispatch, { username, password });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <input
+        style={{ padding: 10, marginBottom: 20 }}
+        type="text"
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+      ></input>
+
+      <input
+        style={{ padding: 10, marginBottom: 20 }}
+        type="password"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
+
+      <button onClick={handleClick} style={{ padding: 10, width: 100 }}>
+        Login
+      </button>
+    </div>
+  );
+};
+
+export default Login;
